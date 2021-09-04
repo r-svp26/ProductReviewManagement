bc17dc6b928e9b6c17818e7f23897d4fc542167b
r-svp26 <riteshk6341@gmail.com>
2021-09-04 08:37:23 +0530

    [Ritesh] Add: Welcome to Product Review Management

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
