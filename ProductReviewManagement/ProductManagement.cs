fc44f4f3ac609173f05760527e0572e894f7dd58
r-svp26 <riteshk6341@gmail.com>
2021-09-06 12:10:05 +0530

    [Ritesh] Add: Find top three records

---
+++ ProductReviewManagement/ProductManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    /// <summary>
+    /// UC1- Product Infromation
+    /// </summary>
+    public class ProductManagement
+    {
+        /// <summary>
+        /// UC-2 Top three records
+        /// </summary>
+        /// <param name="reviews"></param>
+        public static void TopRecords(List<ProductReview> reviews)
+        {
+            var record = (from productReviews in reviews
+                          orderby productReviews.Rating descending
+                          select productReviews).Take(3);
+            foreach (var list in record)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " +
+                        "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
+            }
+        }
+    }
+}
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -14,7 +14,7 @@ namespace ProductReviewManagement
             Console.WriteLine("Welcome to Product Review Management");
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
-                new ProductReview(){ ProductID=1,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=1,UserID=1,Rating=10,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=2,UserID=1,Rating=4,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=3,UserID=2,Rating=5,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=4,UserID=2,Rating=4,Review="Good",IsLike=true },
@@ -23,11 +23,11 @@ namespace ProductReviewManagement
                 new ProductReview(){ ProductID=1,UserID=3,Rating=5,Review="Nice",IsLike=true },
                 new ProductReview(){ ProductID=8,UserID=10,Rating=3,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=9,UserID=10,Rating=5,Review="Good",IsLike=true },
-                new ProductReview(){ ProductID=10,UserID=10,Rating=2,Review="Bad",IsLike=true },
+                new ProductReview(){ ProductID=10,UserID=10,Rating=9,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=11,UserID=1,Rating=5,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=12,UserID=1,Rating=5,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=4,UserID=2,Rating=1.5,Review="Bad",IsLike=false },
-                new ProductReview(){ ProductID=14,UserID=5,Rating=3,Review="nice",IsLike=true },
+                new ProductReview(){ ProductID=14,UserID=5,Rating=8,Review="nice",IsLike=true },
                 new ProductReview(){ ProductID=15,UserID=1,Rating=5,Review="Good",IsLike=true },
                 new ProductReview(){ ProductID=5,UserID=10,Rating=2,Review="Not bad",IsLike=false},
                 new ProductReview(){ ProductID=17,UserID=7,Rating=3.5,Review="Nice",IsLike=true },
@@ -37,9 +37,11 @@ namespace ProductReviewManagement
             };
             foreach (var list in productReviewList)
             {
-                Console.WriteLine("ProductID:-"+list.ProductID +" " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " +
-                    "Review:-" + list.Review +" "+"IsLike:-"+list.IsLike );
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " +
+                    "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
             }
+            Console.WriteLine("Top three records");
+            ProductManagement.TopRecords(productReviewList);
         }
     }
 }
